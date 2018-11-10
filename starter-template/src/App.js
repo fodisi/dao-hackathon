321f2350873812b3fd1f631f74226458916d351e
github username <github email address>
2018-11-10 18:04:13 -0500

    added react-router-dom

---
+++ starter-template/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ProposalContainer from './containers/ProposalContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ProposalContainer />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ starter-template/src/containers/ProposalContainer.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import ProposeNewPeep from '../components/ProposeNewPeep';
+import AddPeepToList from '../components/AddPeepToList';
+import GetPeepProposalList from '../components/GetPeepProposalList';
+
+import UpvotePeep from '../components/ProposeNewPeep';
+import DownvotePeep from '../components/AddPeepToList';
+import GetUserReputation from '../components/GetPeepProposalList';
+import GetPeepContentFromHash from '../components/GetPeepContentFromHash';
+
+import voteProposal from '../actions/voteProposal';
+import submitProposal from '../actions/submitProposal';
+import viewProposalList from '../actions/viewProposalList';
+
+class SubmitProposalContainer extends Component {
+  state = {
+    initiative: ''
+  }
+
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+          <h1>BUSHWICH GENERATOR DIGITAL COMMUNE</h1>
+
+           <a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
+
+           <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+             <i class="fas fa-bars"></i>
+           </button>
+
+           <!-- Navbar Search -->
+           <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+             <div class="input-group">
+               <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+               <div class="input-group-append">
+                 <button class="btn btn-primary" type="button">
+                   <i class="fas fa-search"></i>
+                 </button>
+               </div>
+             </div>
+           </form>
+
+           <!-- Navbar -->
+           <ul class="navbar-nav ml-auto ml-md-0">
+             <li class="nav-item dropdown no-arrow mx-1">
+               <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <i class="fas fa-bell fa-fw"></i>
+                 <span class="badge badge-danger">9+</span>
+               </a>
+               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
+                 <a class="dropdown-item" href="#">Action</a>
+                 <a class="dropdown-item" href="#">Another action</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Something else here</a>
+               </div>
+             </li>
+             <li class="nav-item dropdown no-arrow mx-1">
+               <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <i class="fas fa-envelope fa-fw"></i>
+                 <span class="badge badge-danger">7</span>
+               </a>
+               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
+                 <a class="dropdown-item" href="#">Action</a>
+                 <a class="dropdown-item" href="#">Another action</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#">Something else here</a>
+               </div>
+             </li>
+             <li class="nav-item dropdown no-arrow">
+               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 <i class="fas fa-user-circle fa-fw"></i>
+               </a>
+               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                 <a class="dropdown-item" href="#">Settings</a>
+                 <a class="dropdown-item" href="#">Activity Log</a>
+                 <div class="dropdown-divider"></div>
+                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+               </div>
+             </li>
+           </ul>
+         </nav>
+      </div>
+    )
+  }
+}
+
+export default SubmitProposalContainer;
