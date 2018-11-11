40d9a1e96805184a52853fbdb4b291bb11abcdf3
Gabriel Tan <gabriel.v.tan@gmail.com>
2018-11-10 20:02:39 -0500

    final

--- src/components/InitiativeItem.js
+++ src/components/InitiativeItem.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import web3 from '../utils/web3'
+import DAOinstance from '../ethereum/DAOinstance.js'
+class InitiativeIem extends Component {
+   state = {
+       initiatives : []
+   };
+    async componentDidMount () {
+       //const initiatives = await DAOinstance.methods().getInitiatives().call();
+       const initiatives = [{name:"Rooftop Music Party", description: "Party Tonight"}]
+       this.setState({initiatives});
+   }
 
-class InitiativeItem extends Component {
-  render() {
+render() {
+   const initiatives = this.state.initiatives.map(item => {
+       return (
+           <div>
+               <h2>{item.name}</h2>
+               <p>{item.description}</p>
+           </div>
+       )
+   })
     return (
-      <div id="initiative-item">
-        <h2>Rooftop Party</h2>
-        <p>Description.........</p>
+     <div id="proposal-description">
+         {initiatives}
       </div>
-    )
-  }
+   )
+ }
 }
-
-export default InitiativeItem;
+export default InitiativeIem;
